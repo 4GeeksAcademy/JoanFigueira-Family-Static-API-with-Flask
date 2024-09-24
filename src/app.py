40c338823b110fe0a2a5a176ea613bd6475b3d6b
Joan Figueira <joanfigueira365@gmail.com>
2024-09-24 15:45:20 +0000

    feat: Family API

--- src/app.py
+++ src/app.py
@@ -21,18 +21,53 @@ def handle_invalid_usage(error):
 
 
 # Generate sitemap with all your endpoints
-@app.route('/')
+@app.route('/', methods=['GET'])
 def sitemap():
     return generate_sitemap(app)
 
 
-@app.route('/members', methods=['GET'])
+@app.route('/members', methods=['GET', 'POST'])
 def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    response_body = {}
+    if request.method =='GET':
+        members = jackson_family.get_all_members()
+        response_body["message"] = "Listado de miembros de la familia"
+        response_body["family"] = members
+        return response_body, 200
+    if request.method == 'POST':
+        data = request.json
+        print(data)
+        jackson_family.add_member(data)
+        response_body["message"] = "Quiero agregar un member"
+        response_body["family"] = jackson_family.get_all_members()
+        return response_body, 200
+
+@app.route('/members/<int:member_id>', methods=['GET','PUT','DELETE'])
+def member(member_id):
+    response_body = {}
+    if request.method == 'GET':
+        member = jackson_family.get_member(member_id)
+        print(member)
+        if not member:
+            print('me devolvio una lista vacia')
+            response_body["message"] = f"El member_id: {member_id} no existe"
+            response_body["results"] = {}
+            return response_body, 400
+        response_body["message"] = f"mensaje desde el GET con int:member_id: {member_id}"
+        response_body["results"] = member[0]
+        return response_body, 200
+    if request.method == 'PUT':
+        pass
+        response_body["message"] = f"mensaje desde el PUT con int:member_id: {member_id}"
+        response_body["family"] = {}
+        return response_body, 200
+    if request.method == 'DELETE':
+        pass
+        response_body["message"] = "mensaje desde el DELETE  con int:member_id"
+        response_body["family"] = {}
+        return response_body, 200
+
+
 
 
 # This only runs if `$ python src/app.py` is executed
